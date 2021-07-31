23300757770761e739fe4fb1e503a35d9851b009
akbaraadis <akbaradis99@gmail.com>
2021-07-31 16:43:34 +0700

    leak added

--- app/src/main/java/com/akbaradi/tourism/home/HomeFragment.kt
+++ app/src/main/java/com/akbaradi/tourism/home/HomeFragment.kt
@@ -1,6 +1,9 @@
 package com.akbaradi.tourism.home
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +15,7 @@ import com.akbaradi.tourism.core.data.Resource
 import com.akbaradi.tourism.core.ui.TourismAdapter
 import com.akbaradi.tourism.databinding.FragmentHomeBinding
 import com.akbaradi.tourism.detail.DetailTourismActivity
+import kotlinx.android.synthetic.main.fragment_home.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
@@ -20,6 +24,7 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var broadcastReceiver: BroadcastReceiver
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -64,6 +69,37 @@ class HomeFragment : Fragment() {
                 adapter = tourismAdapter
             }
         }
+    }
+
+    private fun registerBroadCastReceiver() {
+        broadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                when (intent.action) {
+                    Intent.ACTION_POWER_CONNECTED -> {
+                        tv_power_status.text = getString(R.string.power_connected)
+                    }
+                    Intent.ACTION_POWER_DISCONNECTED -> {
+                        tv_power_status.text = getString(R.string.power_disconnected)
+                    }
+                }
+            }
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+        context?.registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadCastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        context?.unregisterReceiver(broadcastReceiver)
     }
 
     override fun onDestroyView() {
